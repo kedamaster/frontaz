59b19c9d4ffb4017b6215023296ee69fa0cb0231
kedamaster <mrkmatambanadzo123@gmail.com>
2024-07-29 11:00:06 +0200

    spects files

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+selector: 'app-navbar',
+standalone: true,
+imports: [CommonModule],
+templateUrl: './navbar.component.html',
+styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  isDropdownOpen: boolean = false;
+isDropdownOpen: boolean = false;
 
-  toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
-  }
+toggleDropdown() {
+this.isDropdownOpen = !this.isDropdownOpen;
+}
 }
