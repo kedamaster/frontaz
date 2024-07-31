fa5ad8fb0fbe2129a85b084b1d55886934792b54
Addynene <Addynene.chikukwaadleen@gmail.com>
2024-07-31 03:46:36 -0400

    counter

--- src/app/doctors/doctors.component.ts
+++ src/app/doctors/doctors.component.ts
@@ -9,4 +9,50 @@ import { Component } from '@angular/core';
 })
 export class DoctorsComponent {
 
+  doctorscount:number = 0;
+
+  doctorscountstop:any = setInterval(()=>{
+    this.doctorscount++;
+
+    if(this.doctorscount == 20)
+    {
+      clearInterval(this.doctorscountstop);
+    }
+  },100)//milliseconds
+
+
+
+  experiencecount:number = 0;
+
+  experiencecountstop:any = setInterval(()=>{
+    this.experiencecount++;
+
+    if(this.experiencecount == 11)
+    {
+      clearInterval(this.experiencecountstop);
+    }
+  },100)//milliseconds
+
+  casualtiescount:number = 0;
+
+  casualtiescountstop:any = setInterval(()=>{
+    this.casualtiescount++;
+
+    if(this.casualtiescount == 300)
+    {
+      clearInterval(this.casualtiescountstop);
+    }
+  },100)//milliseconds
+
+  countriescount:number = 0;
+
+  countriescountstop:any = setInterval(()=>{
+    this.countriescount++;
+
+    if(this.countriescount == 3)
+    {
+      clearInterval(this.countriescountstop);
+    }
+  },100)//milliseconds
+
 }
